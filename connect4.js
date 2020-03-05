2b65455aa093e20d09ed317da10402b70ea86d88
Charles Kantz <kantz.charles@gmail.com>
2020-03-05 15:23:10 -0800

    clearing TODOs and refectoring stuff

--- connect4.js
+++ connect4.js
@@ -16,7 +16,7 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  // old bad array creation that had memory sharing issues
   // board = new Array(HEIGHT).fill(new Array(WIDTH).fill(null));
   
   for(let i = 0; i < HEIGHT; i++){
@@ -26,13 +26,12 @@ function makeBoard() {
     }
     board.push(boardRow);
   }
-
+  // TODO: play with Array.fill
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   let htmlBoard = document.getElementById('board');
 
   // create top row to place clickable cells to place game pieces on board
@@ -67,9 +66,8 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  for(let i = HEIGHT - 1; i >= 0; i--){
-    if(board[i][x] === null){
+  for (let i = HEIGHT - 1; i >= 0; i--) {
+    if (board[i][x] === null) {
       return i;
     }
   }
@@ -79,10 +77,8 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
   let gamePiece = document.createElement('div');
-  gamePiece.classList.add('piece');
-  gamePiece.classList.add(`p${currPlayer}`);
+  gamePiece.classList.add('piece', `p${currPlayer}`);
   let cellCoords = document.getElementById(`${y}-${x}`)
   cellCoords.append(gamePiece);
 }
@@ -90,7 +86,6 @@ function placeInTable(y, x) {
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
   alert(msg);
 }
 
@@ -107,7 +102,6 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
   board[y][x] = currPlayer;
 
@@ -117,17 +111,17 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  if(board.every((array) => {
+ 
+  let isTied = board.every((array) => {
     return array.every((cell) => {
       return cell !== null;
     });
-  })){
-    return endGame(`The game tied, you both suck.`);
+  });
+  if(isTied){
+    return endGame(`The game tied!`);
   }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
   currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
 }
 
